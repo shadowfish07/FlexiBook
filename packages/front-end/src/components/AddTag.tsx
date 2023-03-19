6e3d9dee211d5e64ee18ec1ec055135310ef7c59
shadowfish <shadowfish@foxmail.com>
2023-03-19 22:42:25 +0800

    refactor: extract ColorPicker

--- packages/front-end/src/components/AddTag.tsx
+++ packages/front-end/src/components/AddTag.tsx
@@ -6,11 +6,11 @@ import {
   TreeSelect,
   Tag as AcroTag,
 } from "@arco-design/web-react";
-import { IconCheck, IconPlus } from "@arco-design/web-react/icon";
+import { IconPlus } from "@arco-design/web-react/icon";
 import { useState } from "react";
 import styled from "styled-components";
 import { useStorage } from "../hooks";
-import { useDataState } from "../store/useDataState";
+import { ColorPicker } from "./ColorPicker";
 import { Tag } from "./Tag";
 
 type Props = {
@@ -24,88 +24,6 @@ const StyledContent = styled.div`
     justify-content: flex-end;
   }
 `;
-
-const StyledColorItem = styled.div<{ color: string }>`
-  width: 20px;
-  height: 20px;
-  border-radius: 50%;
-  background-color: ${(props) => props.color};
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  &:hover {
-    box-shadow: 0 0 0 2px #eaf4ff;
-  }
-`;
-
-const StyledColorPicker = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  width: 100%;
-  margin-top: 10px;
-`;
-
-type ColorPickerProps = {
-  onSelect?: (color: string) => void;
-  defaultColor?: string;
-};
-
-type ColorItemProps = {
-  color: string;
-  isSelected: boolean;
-  onClick: (color: string) => void;
-};
-
-function ColorPicker({ defaultColor, onSelect }: ColorPickerProps) {
-  const colors = [
-    "#CB2E34",
-    "#CC5729",
-    "#D26913",
-    "#CC961F",
-    "#CFB325",
-    "#84B723",
-    "#129A37",
-    "#1FA6AA",
-    "#2971CF",
-    "#1D4DD2",
-    "#5A25B0",
-    "#B01BB6",
-    "#CB2B88",
-  ];
-
-  const [selectedColor, setSelectedColor] = useState<string>(
-    defaultColor ?? colors[0]
-  );
-
-  const ColorItem = ({ color, isSelected, onClick }: ColorItemProps) => {
-    return (
-      <StyledColorItem color={color} onClick={() => onClick(color)}>
-        {isSelected && <IconCheck style={{ color: "#ffffff" }} />}
-      </StyledColorItem>
-    );
-  };
-
-  const handleColorClick = (color: string) => {
-    setSelectedColor(color);
-    onSelect && onSelect(color);
-  };
-
-  return (
-    <StyledColorPicker>
-      <Space wrap size={6}>
-        {colors.map((color) => (
-          <ColorItem
-            color={color}
-            onClick={handleColorClick}
-            isSelected={selectedColor === color}
-          />
-        ))}
-      </Space>
-    </StyledColorPicker>
-  );
-}
 
 function CreateNewTag() {
   const [name, setName] = useState<string>("");
