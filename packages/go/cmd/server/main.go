cd48f9997c124f9aa8d10150e7234d5e9f36fd91
shadowfish <shadowfish@foxmail.com>
2023-03-30 00:10:11 +0800

    refactor: remove unused code

--- packages/go/cmd/server/main.go
+++ packages/go/cmd/server/main.go
@@ -10,66 +10,6 @@ func setupRouter() *gin.Engine {
 
 	routes.RegisterRoutes(router)
 
-	// result.GET("/website/meta", func(ctx *gin.Context) {
-	// 	url := ctx.Query("url")
-
-	// 	if url == "" {
-	// 		ctx.JSON(http.StatusBadRequest, gin.H{
-	// 			"error": "url is required",
-	// 		})
-	// 		return
-	// 	}
-
-	// 	resp, err := http.Get(url)
-	// 	if err != nil {
-	// 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	// 		return
-	// 	}
-	// 	defer resp.Body.Close()
-
-	// 	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	// 	if err != nil {
-	// 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	// 		return
-	// 	}
-
-	// 	title := doc.Find("head > title").First().Text()
-	// 	description := doc.Find("head > meta[name=description]").First().AttrOr("content", "")
-	// 	ctx.JSON(http.StatusOK, gin.H{"title": title, "description": description})
-	// })
-
-	// result.GET("/website/icons", func(ctx *gin.Context) {
-	// 	url := ctx.Query("url")
-
-	// 	if url == "" {
-	// 		ctx.JSON(http.StatusBadRequest, gin.H{
-	// 			"error": "url is required",
-	// 		})
-	// 		return
-	// 	}
-
-	// 	resp, err := http.Get(url)
-	// 	if err != nil {
-	// 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	// 		return
-	// 	}
-	// 	defer resp.Body.Close()
-
-	// 	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	// 	if err != nil {
-	// 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	// 		return
-	// 	}
-
-	// 	body, err := parse.GetIcon(doc, url)
-	// 	if err != nil {
-	// 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	// 		return
-	// 	}
-
-	// 	ctx.Data(http.StatusOK, "image/x-icon", body)
-	// })
-
 	return router
 }
 
