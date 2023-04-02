f9ca966eaa5c962c136f7c400cd4f112e83b98a4
shadowfish <shadowfish@foxmail.com>
2023-04-02 12:40:31 +0800

    refactor: storage 职责单纯

--- packages/go/storage/database.go
+++ packages/go/storage/database.go
@@ -1,37 +1,76 @@
 package storage
 
 import (
+	"errors"
+
+	"github.com/goccy/go-json"
 	"github.com/shadowfish07/FlexiBook/models"
 )
 
 type Database struct {
+	storage          *Storage
+	cache            *models.Database
+	DatabaseFileName string
+	hasInit          bool
+}
+
+func init() {
+	CachedDatabase = NewDatabase()
 }
 
 func NewDatabase() *Database {
-	return &Database{}
+	return &Database{cache: &models.Database{
+		Tags:       make(models.Tags),
+		Categories: make(models.Categories),
+		Bookmarks:  make(models.Bookmarks),
+	},
+		DatabaseFileName: "database.json",
+		hasInit:          false,
+		storage:          NewStorage(),
+	}
 }
 
 func (dr *Database) NewDatabase() *models.Database {
 	return &models.Database{}
 }
 
-// func (dr *Database) SetTags(value models.Tags) error {
-// 	data, err := dr.Get()
-// 	if err != nil {
-// 		return err
-// 	}
-// 	data.Tags = value
-// 	return CachedStorage.Save(data)
-// }
-
 func (dr *Database) Save(value *models.Database) error {
-	return CachedStorage.Save(value)
+	jsonData, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+
+	return dr.storage.Save(dr.DatabaseFileName, jsonData)
 }
 
-func (dr *Database) Load() error {
-	return CachedStorage.Load()
+func (dr *Database) load() error {
+	fileData, err := dr.storage.Load(dr.DatabaseFileName)
+	if err != nil {
+		return err
+	}
+
+	dr.hasInit = true
+
+	if fileData == nil || len(fileData) == 0 {
+		return nil
+	}
+
+	err = json.Unmarshal(fileData, dr.cache)
+	if err != nil {
+		return errors.New("database file is broken")
+	}
+
+	return nil
 }
 
 func (dr *Database) Get() (*models.Database, error) {
-	return CachedStorage.Get()
+	if !dr.hasInit {
+		err := dr.load()
+		if err != nil {
+			return nil, err
+		}
+	}
+	return dr.cache, nil
 }
+
+var CachedDatabase *Database
--- packages/go/storage/storage.go
+++ packages/go/storage/storage.go
@@ -1,52 +1,30 @@
 package storage
 
 import (
-	"errors"
 	"io"
 	"os"
 
-	"github.com/goccy/go-json"
 	"github.com/shadowfish07/FlexiBook/config"
-	"github.com/shadowfish07/FlexiBook/models"
 )
 
 type Storage struct {
-	hasInit          bool
-	cache            *models.Database
-	MountDir         string
-	DatabaseFileName string
-}
-
-func init() {
-	CachedStorage = NewStorage()
-	CachedStorage.Load()
+	MountDir string
 }
 
 func NewStorage() *Storage {
 	return &Storage{
-		cache: &models.Database{
-			Tags:       make(models.Tags),
-			Categories: make(models.Categories),
-			Bookmarks:  make(models.Bookmarks),
-		},
-		MountDir:         config.MountDir,
-		DatabaseFileName: "database.json",
+		MountDir: config.MountDir,
 	}
 }
 
-func (s *Storage) Save(newContext *models.Database) error {
-	jsonData, err := json.Marshal(newContext)
-	if err != nil {
-		return err
-	}
-
+func (s *Storage) Save(fileName string, newContext []byte) error {
 	// 避免文件目录不存在
-	err = os.MkdirAll(s.MountDir, 0755)
+	err := os.MkdirAll(s.MountDir, 0755)
 	if err != nil {
 		return err
 	}
 
-	filePath := s.MountDir + "/" + s.DatabaseFileName
+	filePath := s.MountDir + "/" + fileName
 
 	file, err := os.Create(filePath)
 	if err != nil {
@@ -55,7 +33,7 @@ func (s *Storage) Save(newContext *models.Database) error {
 
 	defer file.Close()
 
-	_, err = file.WriteString(string(jsonData))
+	_, err = file.Write(newContext)
 	if err != nil {
 		return err
 	}
@@ -68,52 +46,21 @@ func (s *Storage) Save(newContext *models.Database) error {
 	return nil
 }
 
-func (s *Storage) Load() error {
+func (s *Storage) Load(fileName string) ([]byte, error) {
 	// 避免文件目录不存在
 	err := os.MkdirAll(s.MountDir, 0755)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	filePath := s.MountDir + "/" + s.DatabaseFileName
+	filePath := s.MountDir + "/" + fileName
 
 	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_RDWR, 0644)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	defer file.Close()
 
-	fileInfo, err := file.Stat()
-	if err != nil {
-		return err
-	}
-
-	if fileInfo.Size() != 0 {
-		fileData, err := io.ReadAll(file)
-		if err != nil {
-			return err
-		}
-
-		err = json.Unmarshal(fileData, s.cache)
-		if err != nil {
-			return errors.New("database file is broken")
-		}
-	}
-
-	s.hasInit = true
-
-	return nil
+	return io.ReadAll(file)
 }
-
-func (s *Storage) Get() (*models.Database, error) {
-	if !s.hasInit {
-		err := s.Load()
-		if err != nil {
-			return nil, err
-		}
-	}
-	return s.cache, nil
-}
-
-var CachedStorage *Storage
