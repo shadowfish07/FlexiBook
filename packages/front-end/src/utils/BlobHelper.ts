9bd03bb66fd72dd266c6e71e30e081165903de04
shadowfish <shadowfish@foxmail.com>
2023-04-08 19:03:34 +0800

    fix: blob

--- packages/front-end/src/utils/BlobHelper.ts
+++ packages/front-end/src/utils/BlobHelper.ts
@@ -8,13 +8,13 @@ export const saveBlob = (key: BlobKeys, blob: Blob): Promise<string> => {
     const id = nanoid();
     reader.onload = (event) => {
       chrome.storage.local
-        .set({ [BLOB_KEY]: { [`${id}`]: event?.target?.result } })
+        .set({ [key]: { [`${id}`]: event?.target?.result } })
         .then(() => resolve(`${id}`));
     };
 
     reader.readAsDataURL(blob);
   });
 };
-export const loadBlob = async (key: string) => {
+export const loadIconBlob = async (key: string) => {
   return (await chrome.storage.local.get(BLOB_KEY))[BLOB_KEY][key];
 };
